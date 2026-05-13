6a983c9d870a66cf3d1079fdd20db017b97fa19c
Nikita <n.g3nnadevich@gmail.com>
2026-05-13 18:32:37 +0300

    fulfilled eslint's recommendations

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import parse from './parse.js';
-import compareFiles from './compareFiles.js';
+import parse from './parse.js'
+import compareFiles from './compareFiles.js'
 
 const genDiff = (filepath1, filepath2) => {
-  const parsePath1 = parse(filepath1);
-  const parsePath2 = parse(filepath2);
+  const parsePath1 = parse(filepath1)
+  const parsePath2 = parse(filepath2)
 
-  return compareFiles(parsePath1, parsePath2);
-};
+  return compareFiles(parsePath1, parsePath2)
+}
 
-export default genDiff;
+export default genDiff
