8308d72e423e92cd9174a9757edb8f2af2d19f7f
Nikita <n.g3nnadevich@gmail.com>
2026-05-13 18:19:42 +0300

    update README and add coverage in .gitignore

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const genDiff = (filepath1, filepath2) => {
   const parsePath1 = parse(filepath1);
   const parsePath2 = parse(filepath2);
 
-  return compareFiles(parsePath1, parsePath1);
+  return compareFiles(parsePath1, parsePath2);
 };
 
 export default genDiff;
