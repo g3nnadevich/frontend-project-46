3dff86033ff0ec09f3c96124b40a476d927916e1
Nikita <n.g3nnadevich@gmail.com>
2026-05-14 12:42:13 +0300

    rename folders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import parse from './parse.js'
+import parse from './parses.js'
 import compareFiles from './compareFiles.js'
 
-const genDiff = (filepath1, filepath2) => {
+export default (filepath1, filepath2) => {
   const parsePath1 = parse(filepath1)
   const parsePath2 = parse(filepath2)
 
   return compareFiles(parsePath1, parsePath2)
 }
-
-export default genDiff
