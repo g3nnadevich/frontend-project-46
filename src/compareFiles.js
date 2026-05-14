36a04ce469ec41981ebb4aa3e33417cb24b5ce38
Nikita <n.g3nnadevich@gmail.com>
2026-05-14 22:50:28 +0300

    Add recursive comparison

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -6,19 +6,104 @@ const compareFiles = (file1, file2) => {
   const sortedKeys = _.sortBy(_.union(keys1, keys2))
 
   const result = sortedKeys.map((key) => {
-    if (_.has(file1, key) && _.has(file2, key)) {
-      if (file1[key] === file2[key]) {
-        return `${key}: ${file1[key]}`
+    if(!_.has(file1, key)) {
+      return {
+        name: key,
+        value: file2[key],
+        type: 'added',
       }
-      return [`- ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`]
     }
-    if (_.has(file1, key)) {
-      return `- ${key}: ${file1[key]}`
+    if(!_.has(file2, key)) {
+      return {
+        name: key,
+        value: file1[key],
+        type: 'removed',
+      }
     }
-    return `+ ${key}: ${file2[key]}`
+    if(_.isPlainObject(file1[key]) && _.isPlainObject(file2[key])) {
+      return {
+        name: key,
+        children: compareFiles(file1[key], file2[key]),
+        type: 'nested',
+      }
+    }
+    if(file1[key] !== file2[key]) {
+      return {
+        name: key,
+        oldValue: file1[key],
+        newValue: file2[key],
+        type: 'changed',
+      }
+    }
+    return {
+      name: key,
+      value: file1[key],
+      type: 'unchanged',
+    }
   })
-
-  return `{\n  ${result.join('\n  ')}\n}`
+  return result;
 }
 
-export default compareFiles
+export default compareFiles;
+
+/* 
+{
+  "common": {
+    "setting1": "Value 1",
+    "setting2": 200,
+    "setting3": true,
+    "setting6": {
+      "key": "value",
+      "doge": {
+        "wow": ""
+      }
+    }
+  },
+  "group1": {
+    "baz": "bas",
+    "foo": "bar",
+    "nest": {
+      "key": "value"
+    }
+  },
+  "group2": {
+    "abc": 12345,
+    "deep": {
+      "id": 45
+    }
+  }
+}
+
+{
+  "common": {
+    "follow": false,
+    "setting1": "Value 1",
+    "setting3": null,
+    "setting4": "blah blah",
+    "setting5": {
+      "key5": "value5"
+    },
+    "setting6": {
+      "key": "value",
+      "ops": "vops",
+      "doge": {
+        "wow": "so much"
+      }
+    }
+  },
+  "group1": {
+    "foo": "bar",
+    "baz": "bars",
+    "nest": "str"
+  },
+  "group3": {
+    "deep": {
+      "id": {
+        "number": 45
+      }
+    },
+    "fee": 100500
+  }
+}
+
+*/
