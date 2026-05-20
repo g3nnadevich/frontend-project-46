1b79da597edb55936aa0323c116aca0a16ef10b3
Nikita <n.g3nnadevich@gmail.com>
2026-05-20 12:32:14 +0300

    update tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -21,7 +21,7 @@ test.each(files)('gendiff flat %s', (_, filename1, filename2) => {
 
   const stylishExpected = readFile('expected_stylish.txt').trim()
   const plainExpected = readFile('expected_plain.txt').trim()
-  const jsonExpected = readFile('expected_plain.txt').trim()
+  const jsonExpected = readFile('expected_json.txt').trim()
   
   const stylishResult = genDiff(fixture1Path, fixture2Path, "stylish")
   const plainResult = genDiff(fixture1Path, fixture2Path, "plain")
